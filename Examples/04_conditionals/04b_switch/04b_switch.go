9ce3cc8a4557bdf48b3b5f98593363001ca20062
Mathieu Humbert <mathieu.humbert@accenture.com>
2023-04-27 17:21:53 +0200

    Add Switch

---
+++ Examples/04_conditionals/04b_switch/04b_switch.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//This one is simply taken from https://gobyexample.com/switch
+
+func main() {
+
+	// ➡️ Regular switch for a specific variable i
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// ➡️ Regular switch for a value resulting of a function call
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// ➡️ Empty switch. Works exactly like if/else
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// ➡️ Switch for type checking
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
