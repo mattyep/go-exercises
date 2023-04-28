d8d04159d0c7f5ceb330c3fe676d61cd25e20b58
Christelle Lozachmeur <c.lozachmeur@accenture.com>
2023-04-28 13:51:04 +0200

    balec2

--- 03_for_loop/03a_for_loop/for_loop_test.go
+++ 03_for_loop/03a_for_loop/for_loop_test.go
@@ -18,32 +18,69 @@ func arrayWith1to5() [5]int {
 	return array
 }
 
+func arrayWith1to5bis() [5]int {
+	var array [5]int
+	//array -> [0 0 0 0 0]
+	for i := 0; i <= 4; i++ {
+		array[i] = i + 1
+	}
+	return array
+}
+
 // 2️⃣ - Complete the following method with a for-loop of your choice so that
 // the returned array contains the values 1 to n ([1 2 ... n])
 func arrayWith1toN(n int) []int {
 
-	var array []int = make([]int, n)
+	var slice []int
+	// slice -> [] -> nil
+	slice = make([]int, n)
+	// slice -> [0 0 0 ... 0] n times
+	// len(array) == n
 
-	for i := 0; i < len(array); i++ {
-		fmt.Println(array[i])
+	//  for index := 0; index < n; index++ {
+	// 		slice[index] += 1 + index
+	//  	//slice = append(slice, index + 1)
+	// 	fmt.Println(array)
+	//  }
+	for index, valeur := range slice {
+		slice[index] = (1 + index)
+		fmt.Println(valeur)
 	}
-	return array
+	return slice
 
 }
 
 // 3️⃣ - Complete the following method with a for-loop of your choice so that
 // the sum of the array is returned (e.g. [1 2 3] -> 6)
 func arraySum(array []int) int {
-	//...
-	return 0
+
+	//var tabArray = []int{1, 2, 3}
+
+	sum := 0
+
+	for index, valeur := range array {
+		sum += valeur
+		fmt.Println(index, valeur)
+
+	}
+	// for index := 0; index <= 2; index++ {
+	// 	fmt.Println(sum)
+	// }
+
+	return sum
+
 }
 
 // 4️⃣ - Complete the following method with a for-loop of your choice so that
 // it squares all the values (value * value) and return them on an array of the same size (e.g. [1 2 3] -> [1 4 9])
 func arraySquare(array []int) []int {
-	var result []int
-	//...
-	return result
+	var result = []int{1, 2, 3}
+
+	for index, valeur := range array {
+
+		fmt.Println(result)
+	}
+	// return result
 }
 
 // 5️⃣ - Complete the following method with a for-loop of your choice so that
@@ -61,7 +98,7 @@ func arrayRotateLeft(array []int) []int {
 
 func TestArrayWith1to5(t *testing.T) {
 	expected := [5]int{1, 2, 3, 4, 5}
-	if arr := arrayWith1to5(); !reflect.DeepEqual(arr, expected) {
+	if arr := arrayWith1to5bis(); !reflect.DeepEqual(arr, expected) {
 		t.Errorf("arrayWith1to5 returned %v instead of %v", arr, expected)
 	}
 }
@@ -84,7 +121,7 @@ func TestArraySquare(t *testing.T) {
 	input := []int{1, 2, 3}
 	expected := []int{1, 4, 9}
 	if arr := arraySquare(input); !reflect.DeepEqual(arr, expected) {
-		t.Errorf("arraySqaure returned %v instead of %v", arr, expected)
+		t.Errorf("arraySquare returned %v instead of %v", arr, expected)
 	}
 }
 
