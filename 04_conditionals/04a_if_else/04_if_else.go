62ce116f68dce5f3bb21d180ef19cbfc336169ec
yannick.hugues <yannick.hugues@accenture.com>
2023-06-01 14:40:12 +0200

    conditionals

--- 04_conditionals/04a_if_else/04_if_else.go
+++ 04_conditionals/04a_if_else/04_if_else.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // FizzBuzz
 // Write a program that prints the numbers from 1 to 100.
 // But for multiples of three print "Fizz" instead of the number
@@ -7,5 +9,15 @@ package main
 // For numbers which are multiples of both three and five print "FizzBuzz".
 // For more details : https://codingdojo.org/kata/FizzBuzz/
 func main() {
-
+	for i := 0; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 && i != 0 {
+			fmt.Printf("FizzBuzz\n")
+		} else if i%3 == 0 && i != 0 {
+			fmt.Printf("Fizz\n")
+		} else if i%5 == 0 && i != 0 {
+			fmt.Printf("Buzz\n")
+		} else {
+			fmt.Printf("%d\n", i)
+		}
+	}
 }
--- 04_conditionals/04b_switch/04_switch.go
+++ 04_conditionals/04b_switch/04_switch.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // FizzBuzz again, but with a switch case now :)
 // Write a program that prints the numbers from 1 to 100.
 // But for multiples of three print "Fizz" instead of the number
@@ -7,5 +9,16 @@ package main
 // For numbers which are multiples of both three and five print "FizzBuzz".
 // For more details : https://codingdojo.org/kata/FizzBuzz/
 func main() {
-
+	for i := 0; i <= 100; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0 && i != 0:
+			fmt.Printf("FizzBuzz\n")
+		case i%3 == 0 && i != 0:
+			fmt.Printf("Fizz\n")
+		case i%5 == 0 && i != 0:
+			fmt.Printf("Buzz\n")
+		default:
+			fmt.Printf("%d\n", i)
+		}
+	}
 }
